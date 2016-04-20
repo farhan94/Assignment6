7f82293ab0142d85906143c211f95f1d624a9073
Farhan Ali <farhan.ali@utexas.edu>
2016-04-20 14:11:50 -0500

    fixed pathing and added a few things

---
+++ ConcertHallSeats.java
@@ -0,0 +1,111 @@
+package assignment6;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ConcertHallSeats {
+	List<Seat> seats; // stores all available seats
+	Lock seatLock; //lock to only allow one thread to access seats at a given time
+	
+	/**
+	 * Creates an ArrayList that holds all availabe seats for the concert hall
+	 */
+	public ConcertHallSeats(){
+		
+		seats = new ArrayList<Seat>();
+		seatLock = new ReentrantLock();
+		ArrayList<Character> row = new ArrayList<Character>();
+		ArrayList<String> sect = new ArrayList<String>();
+		ArrayList<Integer> num = new ArrayList<Integer>();
+		// add all possible rows to list
+		for (char i = 'A'; i <='Z'; i++){
+			row.add(i);
+		}
+		
+		// iterate through each row (A-Z) and add seats to ArrayList
+		Iterator<Character> r = row.iterator();
+		while (r.hasNext()){
+			char x = r.next();
+			// add all available seats (101-128) to ArrayList
+			for (int i = 101; i <129; i++){
+				if(i >= 101 && i<=108){
+					seats.add(new Seat("Right", x, i));
+				}
+				else if(i >= 109 && i<=121){
+					seats.add(new Seat("Middle", x, i));
+				}
+				else if(i >109){
+					seats.add(new Seat("Left", x, i));
+				}
+			}
+		}
+		
+	}
+
+	/**
+	 * finds the best seat available (middle section before left and right, rows closer to stage before rows farther away)
+	 * @return Seat object representing best seat available
+	 */
+	public Seat getBest(){
+		Iterator<Seat> i= seats.iterator();
+		Seat result = null;
+		if(i.hasNext()){
+			result = i.next();
+		}
+		// iterate through all available seats and find best by comparing each 
+		while (i.hasNext()){
+			Seat next = i.next();
+			result = compareSeat(result, next);
+		}
+		return result;
+	}
+	
+	/**
+	 * compares two seats to determine which is better
+	 * @param x - first seat to be compared
+	 * @param y - second seat to be compared
+	 * @return Seat object representing better of two Seats
+	 */
+	public Seat compareSeat(Seat x, Seat y){
+	    // the seat with the lower row number is closer and therefore better
+		if (x.row < y.row){
+			return x;
+		}
+		else if (x.row > y.row){
+			return y;
+		}
+		// middle section is better than right or left section
+		if (x.section.equals("Middle") && !(x.section.equals(y.section))){
+			return x;
+		}
+		else if (y.section.equals("Middle") && !(y.section.equals(x.section))){
+			return y;
+		}
+		return x; 
+	}
+
+	/**
+	 * Removes a Seat from ArrayList
+	 * @param x - Seat to be removed
+	 * @return true if Seat was removed successfully, false otherwise
+	 */
+	protected boolean removeSeat(Seat x){
+		return seats.remove(x);
+	}
+}
+
+
+class Seat {
+	String section; //section (middle, left, or right)
+	char row; //seat row (A-Z)
+	int num; //seat number (101-128)
+	
+	public Seat(String s, char r, int n){
+		section = s;
+		row = r;
+		num = n;
+	}
+}
---
+++ TestTicketOffice.java
@@ -0,0 +1,205 @@
+package assignment6;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class TestTicketOffice {
+
+	public static int score = 0;
+
+/*	 @Test
+	public void basicServerTest() {
+		try {
+			TicketServer.start(16789, "Box Office A");
+		} catch (Exception e) {
+ 			fail();
+ 		}
+ 		TicketClient client = new TicketClient(16789);
+ 		client.requestTicket();
+ 	}
+ 
+ 	@Test
+ 	public void testServerCachedHardInstance() {
+ 		try {
+ 			TicketServer.start(16790, "Box Office A");
+ 		} catch (Exception e) {
+ 			fail();
+ 		}
+ 		TicketClient client1 = new TicketClient("c1", 16790);
+ 		TicketClient client2 = new TicketClient("c2", 16790);
+ 		client1.requestTicket();
+ 		client2.requestTicket();
+ 		
+ 	}
+
+/* 	@Test
+ 	public void twoNonConcurrentServerTest() {
+ 		try {
+ 			TicketServer.start(16791, "Box Office A");
+ 		} catch (Exception e) {
+ 			fail();
+ 		}
+ 		TicketClient c1 = new TicketClient("nonconc1", 16791);
+ 		TicketClient c2 = new TicketClient("nonconc2", 16791);
+ 		TicketClient c3 = new TicketClient("nonconc3", 16791);
+ 		c1.requestTicket();
+ 		c2.requestTicket();
+ 		c3.requestTicket();
+ 	}
+ 
+ 	@Test
+ 	public void twoConcurrentServerTest() {
+ 		try {
+ 			TicketServer.start(16792, "Box Office A");
+ 		} catch (Exception e) {
+ 			fail();
+ 		}
+ 		final TicketClient c1 = new TicketClient("conc1", 16792);
+ 		final TicketClient c2 = new TicketClient("conc2", 16792);
+ 		final TicketClient c3 = new TicketClient("conc3", 16792);
+ 		Thread t1 = new Thread() {
+ 			public void run() {
+ 				c1.requestTicket();
+ 			}
+ 		};
+ 		Thread t2 = new Thread() {
+ 			public void run() {
+ 				c2.requestTicket();
+ 			}
+ 		};
+ 		Thread t3 = new Thread() {
+ 			public void run() {
+ 				c3.requestTicket();
+ 			}
+ 		};
+ 		t1.start();
+ 		t2.start();
+ 		t3.start();
+ 		try {
+ 			t1.join();
+ 			t2.join();
+ 			t3.join();
+ 		} catch (Exception e) {
+ 			e.printStackTrace();
+ 		}
+		
+
+ 	}
+	
+ 	@Test
+ 	public void soldOutNonConcurrentTest() {
+ 	    try {
+             TicketServer.start(16793, "Box Office A");
+         } catch (Exception e) {
+             fail();
+         }
+ 	    for (int i = 0; i < 800; i++){
+             final TicketClient c1 = new TicketClient("client " + i, 16793);
+             c1.requestTicket();
+             
+ 	    }
+ 	    
+ 	}
+	
+ 	@Test
+ 	public void soldOutConcurrentTest() {
+ 	    try {
+             TicketServer.start(16794, "Box Office A");
+         } catch (Exception e) {
+             fail();
+         }
+ 	    Thread[] threads = new Thread[900];
+ 	    for (int i = 0; i < 900; i++){
+ 	        TicketClient c1 = new TicketClient("client " + i, 16794);
+ 	        threads[i] = new Thread() {
+ 	          public void run() {
+ 	              c1.requestTicket();
+ 	          }
+ 	      };
+ 	    }
+ 	    for (int i = 0; i < 900; i++){
+ 	        threads[i].start();
+ 	    }
+ 	    for (int i = 0; i < 900; i++){
+ 	        try {
+ 	          threads[i].join();
+ 	      } catch (Exception e) {
+ 	          e.printStackTrace();
+ 	      }
+ 	    }
+ 	}
+	
+ 	@Test 
+ 	public void twoServersNonConcurrentTest() {
+ 	     try {
+ 	         TicketServer.start(16794, "Box Office A");
+ 	         TicketServer.start(16795, "Box Office B");
+ 	     } catch (Exception e) {
+ 	         fail();
+ 	     }
+ 	     TicketClient client1 = new TicketClient("client 1", 16794);
+ 	     TicketClient client2 = new TicketClient("client 2", 16795);
+ 	     client1.requestTicket();
+ 	     client2.requestTicket();
+ 	}
+	
+	 @Test
+  public void soldOutTwoServersNonConcurrentTest() {
+      try {
+    	  TicketServer.start(16796, "Box Office A");
+            TicketServer.start(16797, "Box Office B");
+        } catch (Exception e) {
+            fail();
+        }
+      for (int i = 0; i < 800; i++) {
+          final TicketClient c1;
+          if (i%2 == 0) {
+              c1 = new TicketClient("client " + i, 16796);    
+          }
+          else {
+              c1 = new TicketClient("client " + i, 16797);   
+          }
+          
+          c1.requestTicket();
+      } 
+      
+  }
+	
+	 @Test
+  public void soldOutConcurrentTwoServersTest() {
+      try {
+            TicketServer.start(16798, "Box Office A");
+            TicketServer.start(16799, "Box Office B");
+        } catch (Exception e) {
+            fail();
+        }
+      Thread[] threads = new Thread[800];
+      for (int i = 0; i < 800; i++){
+          final TicketClient c1;
+          if (i % 2 == 0){
+              c1 = new TicketClient("client " + i, 16798);
+          }
+          else {
+              c1 = new TicketClient("client " + i, 16799);
+          }
+          threads[i] = new Thread() {
+            public void run() {
+                c1.requestTicket();
+            }
+        };
+      }
+      for (int i = 0; i < 800; i++){
+          threads[i].start();
+      }
+      for (int i = 0; i < 800; i++){
+          try {
+            threads[i].join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+      }
+  }*/
+	
+
+}
